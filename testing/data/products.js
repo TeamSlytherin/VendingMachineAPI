866dd4b5a53e078cc7ee5d7fecf0714d34d89633
Devansh Agarwal <devanshagarwal@pop-os.localdomain>
2021-07-19 16:40:39 +0530

    adding seeder filer to import dummy data

---
+++ testing/data/products.js
@@ -0,0 +1,21 @@
+
+const products = [
+    {
+        title: "Pepsi",
+        price: 30,
+        quantitiy: 100
+    },
+    {
+        title: "Milshake",
+        price: 560,
+        quantitiy: 100
+    },
+    {
+        title: "Normal Tea",
+        price: 790,
+        quantitiy: 100
+    }
+]
+
+
+module.exports = products
