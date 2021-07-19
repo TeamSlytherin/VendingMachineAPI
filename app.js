993d32e7bf64da37bd1f4824919420eb95d8c6ba
gunjanjaju01 <gunjanjajubkn@gmail.com>
2021-07-19 16:44:59 +0530

    JWTtokenBasic

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db')
 const dotenv = require('dotenv')
 const adminRoute = require('./routes/admin');
 const userRoute = require('./routes/user');
+const jwt =  require('jsonwebtoken');
 const app= express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -16,9 +17,66 @@ app.use(cors({credentials: true,origin: true}));
 app.use('/admin',adminRoute);
 app.use('/user',userRoute);
 
+app.get('/api', (req, res) => {
+res.json({
+   message: 'Welcome to the VendingMaachine'
+});
+});
+
+app.post('/api/post', verifyToken, (req,res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if(err) {
+          res.sendStatus(403);
+        } else {
+          res.json({
+            message: 'Post request...',
+            authData
+          });
+        }
+      });
+})
+
+app.post('/api/login', (req, res) => {
+// Mock user or admin
+const user = {
+    id:'admin',
+    username: 'admin',
+    email:'admin@gmail.com'
+}
+
+   jwt.sign({user}, 'secretkey' , { expiresIn: '30s' }, (err,token) => {
+       res.json({
+          token
+       });
+   });
+});
 
 
-const POST = process.env.PORT || 4000;
+// FORMAT OF TOKEN
+// Authorization: First <access_token>
+
+// VerifyToken middleware function
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const firstHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof firstHeader !== 'undefined') {
+     // Split at the space
+     const first = firstHeader.split(' ');
+     // Get token from array
+     const firstToken = first[1];
+     // Set the token
+     req.token = firstToken;
+     // Next middleware
+     next();
+    } else {
+      // Forbidden
+      res.sendStatus(403);
+    }
+  
+  }
+
+const PORT = process.env.PORT || 4000;
 
 const connectServer = async (PORT) => {
     await connectDB()
