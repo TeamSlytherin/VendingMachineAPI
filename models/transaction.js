3f8858aac31514c1b0531da5991f267de76c719f
Devansh Agarwal <devanshagarwal@pop-os.localdomain>
2021-07-20 23:59:38 +0530

    changed in models db

--- models/transaction.js
+++ models/transaction.js
@@ -2,18 +2,21 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema(
   {
-    Orderedproduct: [
-      {
-        name: { type: String, required: true },
-        quantity: { type: Number, required: true },
-        price: { type: Number, required: true },
-        productID: {
-          type: mongoose.Schema.Types.ObjectID,
-          required: true,
-          ref: "Product",
-        },
+    token:{
+        type: String,
+        required: true, 
+    },
+    Orderedproduct: {
+      name: { type: String, required: true },
+      quantity: { type: Number, required: true },
+      price: { type: Number, required: true },
+      productID: {
+        type: mongoose.Schema.Types.ObjectID,
+        required: true,
+        ref: "Product",
       },
-    ],
+    },
+
     amountReceived: {
       type: Number,
       default: 0,
